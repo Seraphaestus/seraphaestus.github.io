037fb5a201153993f8700829e3ac420f1c0145cb
Seraphaestus <seraphaestus@gmail.com>
2024-01-05 01:44:30 +0000

    Additional tooltips
    - Tooltips for variables, component access/swizzling
    - Fix easel proportions warping when area width changes via flow
    - Scrollable code, but breaks tooltip z-level so need to fix that (making it a non-shadow element)

--- shade-for-summer/scripts/code-tooltips.js
+++ shade-for-summer/scripts/code-tooltips.js
@@ -1,6 +1,7 @@
 let highlighterGrammarModified = false
 let showSpecificTooltips = {}
 
+const vectorRegex = /[ibdu]?vec[234]/
 const typeRegex = String.raw`(?:float|double|int|bool|d?mat[234](?:x[234])?|[ibdu]?vec[234]|uint|[iu]?sampler[123]D|[iu]?samplerCube|sampler[12]DShadow|samplerCubeShadow|[iu]?sampler[12]DArray|sampler[12]DArrayShadow|[iu]?sampler2DRect|sampler2DRectShadow|[iu]?samplerBuffer|[iu]?sampler2DMS(?:Array)?|[iu]?samplerCubeArray|samplerCubeArrayShadow|[iu]?image[123]D|[iu]?image2DRect|[iu]?imageCube|[iu]?imageBuffer|[iu]?image[12]DArray|[iu]?imageCubeArray|[iu]?image2DMS(?:Array)?|struct|hvec[234]|fvec[234]|sampler3DRect|filter)`
 
 window.addEventListener("load", () => {
@@ -25,6 +26,10 @@ window.addEventListener("load", () => {
 function modifyHighlighterGrammar(env) {
 	if (highlighterGrammarModified) return;
 	Prism.languages.insertBefore('glsl', 'keyword', {
+		'component_accessor': {
+			pattern: /\b\.[_a-zA-Z]\w*\b/,
+			alias: "punctuation"
+		},
 		'shader_output': {
 			pattern: /\b(?:COLOR)\b/,
 			alias: "keyword"
@@ -40,7 +45,7 @@ function modifyHighlighterGrammar(env) {
 			pattern: /#(?:range|color|ignore)\b/
 		},
 		'reserved': {
-			pattern: /\b(?:gl_.*|long|short|double|sizeof|cast|namespace|using|output)\b/,
+			pattern: /\b(?:gl_\w*|long|short|double|sizeof|cast|namespace|using|output)\b/,
 			alias: "keyword"
 		},
 		'return_type': {
@@ -60,9 +65,12 @@ function modifyHighlighterGrammar(env) {
 		},
 		'operator_ternary': {
 			pattern: /[:]/,
-			alias: "operator",
+			alias: "operator"
 		},
 	});
+	Prism.languages.glsl['variable'] = {
+		pattern: /\b[_a-zA-Z]\w*\b/
+	};
 	// Edited to extract reserved terms and types
 	// keyword =(how?) \b(?:attribute|const|uniform|varying|buffer|shared|coherent|volatile|restrict|readonly|writeonly|atomic_uint|layout|centroid|flat|smooth|noperspective|patch|sample|break|continue|do|for|while|switch|case|default|if|else|subroutine|in|out|inout|void|invariant|precise|discard|return|lowp|mediump|highp|precision|common|partition|active|asm|class|union|enum|typedef|template|this|resource|goto|inline|noinline|public|static|extern|external|interface|half|fixed|unsigned|superp|input|output)\b/
 	highlighterGrammarModified = true;
@@ -82,11 +90,12 @@ function onTokenTooltipAdded(token) {
 	const tooltipWidth = Math.min(512, getTextWidth(longestLine, getCanvasFont(token)));
 	const tooltipLeft = (rect.left + rect.width / 2.0) - tooltipWidth / 2.0;
 	const tooltipRight = (rect.left + rect.width / 2.0) + tooltipWidth / 2.0;
-	const margin = 18;
+	const marginRight = 18;
+	const marginLeft = 50;
 	if (tooltipLeft < parentRect.left) {
-		token.style.setProperty("--offset-x", (parentRect.left - tooltipLeft + margin) + "px");
+		token.style.setProperty("--offset-x", (parentRect.left - tooltipLeft + marginLeft) + "px");
 	} else if (tooltipRight > parentRect.right) {
-		token.style.setProperty("--offset-x", (parentRect.right - tooltipRight - margin) + "px");
+		token.style.setProperty("--offset-x", (parentRect.right - tooltipRight - marginRight) + "px");
 	}
 }
 
@@ -106,25 +115,25 @@ function getCodeTooltip(token) {
 		let prefix = "";
 		let tooltip = "";
 		switch (token.innerText) {
-			case "float": tooltip = "represents a decimal number\ne.g. 1.0, 2.345, 999.999"; break;
-			case "vec2":  tooltip = "represents a 2-dimensional vector\ne.g. 2D coordinate\nIt consists of 2 floats: xy\ne.g. vec2(1.0, 2.0).x  ≡  1.0"; break;
-			case "vec3":  tooltip = "represents a 3-dimensional vector\ne.g. RGB color\nIt consists of 3 floats: xyz aka rgb\ne.g. vec3(1.0, 2.0, 3.0).z  ≡  3.0"; break;
-			case "vec4":  tooltip = "represents a 4-dimensional vector\ne.g. RGBA color with transparency\nIt consists of 4 floats: xyzw aka rgba\ne.g. vec4(1.0, 2.0, 3.0, 4.0).yz  ≡  vec2(2.0, 3.0)"; break;
-			case "int":   tooltip = "represents a whole number\ne.g. 1, 2, 999"; prefix = "[Advanced] "; break;
-			case "ivec2": tooltip = "represents a 2-dimensional vector of whole numbers\nIt consists of 2 ints: xy\ne.g. ivec2(1, 2).x  ≡  1"; prefix = "[Advanced] "; break;
-			case "ivec3": tooltip = "represents a 3-dimensional vector of whole numbers\nIt consists of 3 ints: xyz aka rgb\ne.g. ivec3(1, 2, 3).z  ≡  3"; prefix = "[Advanced] "; break;
-			case "ivec4": tooltip = "represents a 4-dimensional vector of whole numbers\nIt consists of 4 ints: xyzw aka rgba\ne.g. ivec4(1, 2, 3, 4).yz  ≡  ivec2(2, 3)"; prefix = "[Advanced] "; break;
-			case "bool":  tooltip = "represents a boolean value\nA boolean can be either true or false"; break;
-			case "mat2":  tooltip = "represents a 2-dimensional matrix, i.e. a 2x2 array of floats"; prefix = "[Advanced] "; break;
-			case "mat3":  tooltip = "represents a 3-dimensional matrix, i.e. a 3x3 array of floats"; prefix = "[Advanced] "; break;
-			case "mat4":  tooltip = "represents a 4-dimensional matrix, i.e. a 4x4 array of floats"; prefix = "[Advanced] "; break;
+			case "float":  tooltip = "represents a decimal number\ne.g. 1.0, 2.345, 999.999"; break;
+			case "vec2":   tooltip = "represents a 2-dimensional vector\ne.g. 2D coordinate\nIt consists of 2 floats: xy\ne.g. vec2(1.0, 2.0).x  ≡  1.0"; break;
+			case "vec3":   tooltip = "represents a 3-dimensional vector\ne.g. RGB color\nIt consists of 3 floats: xyz aka rgb\ne.g. vec3(1.0, 2.0, 3.0).z  ≡  3.0"; break;
+			case "vec4":   tooltip = "represents a 4-dimensional vector\ne.g. RGBA color with transparency\nIt consists of 4 floats: xyzw aka rgba\ne.g. vec4(1.0, 2.0, 3.0, 4.0).yz  ≡  vec2(2.0, 3.0)"; break;
+			case "int":    tooltip = "represents a whole number\ne.g. 1, 2, 999"; prefix = "[Advanced] "; break;
+			case "ivec2":  tooltip = "represents a 2-dimensional vector of whole numbers\nIt consists of 2 ints: xy\ne.g. ivec2(1, 2).x  ≡  1"; prefix = "[Advanced] "; break;
+			case "ivec3":  tooltip = "represents a 3-dimensional vector of whole numbers\nIt consists of 3 ints: xyz aka rgb\ne.g. ivec3(1, 2, 3).z  ≡  3"; prefix = "[Advanced] "; break;
+			case "ivec4":  tooltip = "represents a 4-dimensional vector of whole numbers\nIt consists of 4 ints: xyzw aka rgba\ne.g. ivec4(1, 2, 3, 4).yz  ≡  ivec2(2, 3)"; prefix = "[Advanced] "; break;
+			case "bool":   tooltip = "represents a boolean value\nA boolean can be either true or false"; break;
+			case "mat2":   tooltip = "represents a 2-dimensional matrix, i.e. a 2x2 array of floats"; prefix = "[Advanced] "; break;
+			case "mat3":   tooltip = "represents a 3-dimensional matrix, i.e. a 3x3 array of floats"; prefix = "[Advanced] "; break;
+			case "mat4":   tooltip = "represents a 4-dimensional matrix, i.e. a 4x4 array of floats"; prefix = "[Advanced] "; break;
+			case "struct": tooltip = "groups variables as one object.\ne.g. struct example { float f; bool b; };\ne.g. example x = example(0.0, true);\nStruct variables can also be declared inline after the struct definition. e.g. struct example {float f;} x;"; prefix = "[Advanced] "; break;
 			default: return "I don't know what you're doing with this but good luck!";
 		}
 		if (token.classList.contains("constructor_type")) {
 			return prefix + "A constructor which takes parameters to create a value of the given type. e.g. vec2 x = vec2(0.0, 1.0);\nThis data type " + tooltip;
 		} else if (token.classList.contains("return_type")) {
-			const nextToken = document.evaluate("following-sibling::span[contains(@class, 'token')]", token, null, XPathResult.FIRST_ORDERED_NODE_TYPE).singleNodeValue;
-			return prefix + `Specifies what type of value the function ${nextToken.innerText} returns\nThis data type ` + tooltip;
+			return prefix + `Specifies what type of value the function ${getNextToken(token).innerText} returns\nThis data type ` + tooltip;
 		} else {
 			return prefix + "A data type which " + tooltip;
 		}
@@ -145,8 +154,7 @@ function getCodeTooltip(token) {
 			case "while": return "[Unavailable] A loop which will keep running the code within it until its condition is no longer true\nWhile loops are not available in WebGL  :(";
 			
 			case "const": return "Modifies a variable to be constant for efficiency\nOnce initialized, it cannot be given a new value";
-			case "uniform": return "Modifies a variable to allow constant values to be externally passed into the shader, using input fields that will be automatically generated above the code box\nUniforms must be declared at the top level, outside of functions\nSupported types: float, vec3 (as color), bool";
-			case "struct": return "[Advanced] A data type which groups variables as one object.\ne.g. struct example { float f; bool b; };\ne.g. example x = example(0.0, true);\nStruct variables can also be declared inline after the struct definition. e.g. struct example {float f;} x;";
+			case "uniform": return "Modifies a variable to allow constant values to be externally passed into the shader, using input fields that will be automatically generated above the code box\nUniforms must be declared at the top level, outside of functions";
 			
 			case "COLOR": return "A vec4 output representing the RGBA color of the given pixel\nCOLOR  ≡  gl_FragColor";
 			case "RATIO":
@@ -191,11 +199,11 @@ function getCodeTooltip(token) {
 	} else if (token.classList.contains("operator") && showSpecificTooltips["operators"]) {
 		// Handle double operators
 		let operator = token.innerText;
-		const prevToken = document.evaluate("preceding-sibling::span[contains(@class, 'token')][1]", token, null, XPathResult.FIRST_ORDERED_NODE_TYPE).singleNodeValue
+		const prevToken = getPrevToken(token);
 		if (prevToken.classList.contains("operator") && (token.innerText == "=" || token.innerText == prevToken.innerText)) {
 			operator = prevToken.innerText + token.innerText;
 		} else {
-			const nextToken = document.evaluate("following-sibling::span[contains(@class, 'token')]", token, null, XPathResult.FIRST_ORDERED_NODE_TYPE).singleNodeValue
+			const nextToken = getNextToken(token);
 			if (nextToken.classList.contains("operator") && (nextToken.innerText == "=" || token.innerText == nextToken.innerText)) {
 				operator = token.innerText + nextToken.innerText;
 			}
@@ -231,6 +239,51 @@ function getCodeTooltip(token) {
 			case "#range": return "A uniform hint that specifies the range of input sliders\ne.g. #range(0, 1, 0.01)  gives the slider a range of 0 to 1 with a step of 0.01\nThe step parameter is optional";
 			case "#color": return "A uniform hint that specifies that a vec3 or vec4 should be interpreted as a color for the purposes of HTML input controls";
 		}
+	} else if (token.classList.contains("component_accessor")) {
+		const prevToken = getPrevToken(token).innerText;
+		const parts = token.innerText.slice(1).split("");
+		if (isSwizzle(parts)) {
+			if (parts.length == 1) return `Accesses the ${parts[0]} component of ${prevToken}`;
+			
+			let components = "";
+			for (let i = 0; i < parts.length; i++) {
+				components += parts[i];
+				if (i < parts.length - 1) components += ", ";
+			}
+			
+			if (getNextToken(token).innerText == "=") {
+				return `Assigns the ${parts.length}D vector on the right of the\nassignment to the (${components}) components of ${prevToken}`;
+			}
+			return `Creates a ${parts.length}D vector from the components (${components}) of ${prevToken}\nThis type of access is called swizzling, as you can mix and match\ne.g. v.xyz  or  v.xxx  or  v.zxy`;
+		} else {
+			return `Accesses the ${token.innerText.slice(1)} field of ${prevToken}`;
+		}
+	} else if (token.classList.contains("variable")) {
+		const variableData = codeVariables[token.innerText];
+		if (variableData.type == "struct") return "A custom data type defined by struct";
+		const type = variableData?.type ?? "unknown";
+		switch (variableData?.varType) {
+			case "Uniform": return `Uniform of type ${type}\nA uniform is a top-level variable which allows values to be externally passed into the shader via autogenerated input fields`;
+			case "Iterator": return `Iterator of type ${type}\nThis variable is defined in a for loop, and represents a number which increments with each loop`;
+			default: return `Variable of type ${type}`;
+		}
 	}
 	return null
+}
+
+function getPrevToken(token) {
+	return document.evaluate("preceding-sibling::span[contains(@class, 'token')][1]", token, null, XPathResult.FIRST_ORDERED_NODE_TYPE).singleNodeValue;
+}
+function getNextToken(token) {
+	return document.evaluate("following-sibling::span[contains(@class, 'token')]", token, null, XPathResult.FIRST_ORDERED_NODE_TYPE).singleNodeValue;
+}
+
+function isSwizzle(parts) {
+	let isXYZW = true;
+	let isRGBA = true;
+	for (let part of parts) {
+		if (part != "x" && part != "y" && part != "z" && part != "w") isXYZW = false;
+		if (part != "r" && part != "g" && part != "b" && part != "a") isRGBA = false;
+	}
+	return isXYZW || isRGBA;
 }
