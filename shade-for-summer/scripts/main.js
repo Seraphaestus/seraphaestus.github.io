ad4dbd43c1a20b574d3c376d620e4e8113e576ad
Seraphaestus <seraphaestus@gmail.com>
2024-01-06 01:04:14 +0000

    Placeholder content

--- shade-for-summer/scripts/main.js
+++ shade-for-summer/scripts/main.js
@@ -54,7 +54,10 @@ const uvShader =
 	"	COLOR = vec4(RATIO, 1.0, 0.5);" +
 	"}";
 const targetImageShaders = {
-	"1": {stickers: {sticker: "stamp.png"}, targetCode: "const float plate_size = 0.95;const vec3 bg_color = vec3(122.0, 58.0, 51.0) / 255.0;const vec3 cake_color = vec3(238.0, 161.0, 73.0) / 255.0;const float cake_size = 0.7;const float cake_rotation = 0.125;const float cake_height = 0.2;const bool do_perspective = true;const float antialiasing = 0.1;float antialiased_step(float a, float b) {return smoothstep(b * (1.0 - antialiasing * 0.1), b * (1.0 + antialiasing * 0.1), a);}float in_circle(vec2 uv, vec2 center, float radius) {return antialiased_step(radius, distance(uv, center));}float in_segment(vec2 uv, vec2 center, float radius, float rotation, float size) {float angle = mod(atan(uv.y - center.y, uv.x - center.x) + rotation * TAU, TAU);return in_circle(uv, center, radius) * antialiased_step(angle, TAU / 6.0) * antialiased_step(TAU / 6.0 + TAU / 9.0, angle);}void main() {gl_FragColor.a = 1.0;vec2 uv = COORD;if (do_perspective) uv.y = (uv.y - 0.5) * 1.25 + 0.5;COLOR.rgb = mix(bg_color, vec3(1.0), in_circle(uv, vec2(0.5), plate_size / 2.0));gl_FragColor.rgb = mix(gl_FragColor.rgb, vec3(0.95), in_circle(uv, vec2(0.5), 0.8 * plate_size / 2.0));const float layers = 64.0;vec2 cake_offset = vec2(0.125);for (float i = 0.0; i < layers; i += 1.0) {gl_FragColor.rgb = mix(gl_FragColor.rgb, cake_color * 0.8, in_segment(uv + cake_offset, vec2(0.5, 0.5 + i * cake_height / layers), cake_size * plate_size / 2.0, cake_rotation, cake_size));}gl_FragColor.rgb = mix(gl_FragColor.rgb, cake_color, in_segment(uv + cake_offset, vec2(0.5, 0.5 + cake_height), cake_size * plate_size / 2.0, cake_rotation, cake_size));}"},
+	"_": {stickers: {stamp: "stamp.png"}},
+	"1": {caption: "Alright, all packed and ready to go! Wish me luck.", stickers: {friend:"2024/stickers/silhouette.png", van:"2024/stickers/van_side.png"}, targetCode: "const vec3 sky_color = vec3(194.0, 237.0, 255.0) / 255.0;const vec3 road_color = vec3(199.0) / 255.0;const vec3 building_color = vec3(255.0, 250.0, 194.0) / 255.0;const float top_cutoff = 0.85;const float bottom_cutoff = 0.15;void main(){COLOR.a=1.0;vec2 coord=(COORD-vec2(0.5))*vec2(0.8,1.15);COLOR.rgb=building_color;if(RATIO.y>top_cutoff){COLOR.rgb=sky_color;}if(RATIO.y<bottom_cutoff){COLOR.rgb=road_color;}}"},
+	"2": {caption: "Isn't she a beaut? I'm glad she'll finally see some proper use.", stickers: {van:"2024/stickers/van_front.png"}, targetCode: "const vec3 sky_color = vec3(194.0, 237.0, 255.0) / 255.0;const vec3 road_color = vec3(199.0) / 255.0;const vec3 building_color = vec3(255.0, 250.0, 194.0) / 255.0;const float top_cutoff = 0.94;const float bottom_cutoff = 0.07;void main(){COLOR.a=1.0;vec2 coord=(COORD-vec2(0.5))*vec2(0.8,1.15);if(abs(coord.x)>abs(coord.y)){COLOR.rgb=building_color;if(coord.x>-0.5&&coord.x<0.5){COLOR.rgb*=0.975;}if(RATIO.y>top_cutoff){COLOR.rgb=sky_color;}if(RATIO.y<bottom_cutoff){COLOR.rgb=road_color;}}else{COLOR.rgb=mix(road_color,sky_color,(sign(coord.y)+1.0)*0.5);}}"},
+	"3": {caption: "Remember how we'd come here after school? It tastes like it was yesterday.", targetCode: "const float plate_size = 0.95;const vec3 bg_color = vec3(122.0, 58.0, 51.0) / 255.0;const vec3 cake_color = vec3(238.0, 161.0, 73.0) / 255.0;const float cake_size = 0.7;const float cake_rotation = 0.125;const float cake_height = 0.2;const bool do_perspective = true;const float antialiasing = 0.1;float antialiased_step(float a, float b) {return smoothstep(b * (1.0 - antialiasing * 0.1), b * (1.0 + antialiasing * 0.1), a);}float in_circle(vec2 uv, vec2 center, float radius) {return antialiased_step(radius, distance(uv, center));}float in_segment(vec2 uv, vec2 center, float radius, float rotation, float size) {float angle = mod(atan(uv.y - center.y, uv.x - center.x) + rotation * TAU, TAU);return in_circle(uv, center, radius) * antialiased_step(angle, TAU / 6.0) * antialiased_step(TAU / 6.0 + TAU / 9.0, angle);}void main() {gl_FragColor.a = 1.0;vec2 uv = COORD;if (do_perspective) uv.y = (uv.y - 0.5) * 1.25 + 0.5;COLOR.rgb = mix(bg_color, vec3(1.0), in_circle(uv, vec2(0.5), plate_size / 2.0));gl_FragColor.rgb = mix(gl_FragColor.rgb, vec3(0.95), in_circle(uv, vec2(0.5), 0.8 * plate_size / 2.0));const float layers = 64.0;vec2 cake_offset = vec2(0.125);for (float i = 0.0; i < layers; i += 1.0) {gl_FragColor.rgb = mix(gl_FragColor.rgb, cake_color * 0.8, in_segment(uv + cake_offset, vec2(0.5, 0.5 + i * cake_height / layers), cake_size * plate_size / 2.0, cake_rotation, cake_size));}gl_FragColor.rgb = mix(gl_FragColor.rgb, cake_color, in_segment(uv + cake_offset, vec2(0.5, 0.5 + cake_height), cake_size * plate_size / 2.0, cake_rotation, cake_size));}"},
 }
 let targetImageShader = null
 
@@ -71,6 +74,7 @@ function setup(event) {
 	if (day in targetImageShaders) {
 		title.textContent = title.textContent.replace("Title", "Day " + day);
 		targetImageShader = targetImageShaders[day]
+		document.querySelector("#postcard-caption").innerText = targetImageShader.caption;
 	} else {
 		title.textContent = title.textContent.replace("Title", "Code Playground");
 	}
@@ -84,17 +88,17 @@ function setup(event) {
 	// Init canvases
 	const paragraph = document.querySelector("p");
 	editor = document.querySelector(".editor");
-	const stickers = targetImageShaders[day]?.stickers ?? {};
+	
+	const stickers = targetImageShaders[day]?.stickers ?? targetImageShaders["_"]?.stickers ?? {};
+	for (let stickerName in stickers) {
+			fragmentSourcePrependString += `uniform sampler2D ${stickerName} #display;\n`;
+			fragmentSourcePrependLineCount++;
+	}
+	
 	if (targetImageShader != null) {
 		targetCanvas = new ShadeableCanvas(document.querySelector("#target-canvas"), paragraph, false);
 		
-		let targetCode = ""
-		for (let stickerName in stickers) {
-			fragmentSourcePrependString += `uniform sampler2D ${stickerName} #display;\n`;
-			fragmentSourcePrependLineCount++;
-		}
-		targetCode += targetImageShaders[day].targetCode;
-		updateCanvas(targetCanvas, targetCode, false, false);
+		updateCanvas(targetCanvas, targetImageShaders[day].targetCode, false, false);
 		
 		for (let stickerName in stickers) {
 			targetCanvas.setSamplerUniform(stickerName, stickers[stickerName]);
@@ -263,15 +267,18 @@ function createCustomUniformInput(source, createInputs = true) {
 			const callback = function(inputs) {
 				const code = codeJar.toString();
 				const codeToInsert = `place_sticker(${varName}, COORD);`;
+				
+				const mainMatch = code.match(/void\s+main\s*\(\s*\)\s*{/);
+				const mainPos = mainMatch.index + mainMatch[0].length;
 				try {
 					const cursorPos = codeJar.save().start;
-					if (cursorPos < code.length) {
+					if (cursorPos < code.length && cursorPos > mainPos) {
 						codeJar.updateCode(code.slice(0, cursorPos) + codeToInsert + code.slice(cursorPos));
-						return
+						return;
 					}
 				} catch(e) {}
-				const mainMatch = code.match(/void\s+main\s*\(\s*\)\s*{/);
-				const cursorPos = mainMatch.index + mainMatch[0].length;
+				
+				const cursorPos = code.length - 2;
 				codeJar.updateCode(code.slice(0, cursorPos) + "\n\t" + codeToInsert + code.slice(cursorPos));
 			}
 			createUniformInput(uniformArea, varName, [attributes], callback);
